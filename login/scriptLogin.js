5dd0f4d757795b9402739b20be29a2fe3d9af001
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-03-02 09:26:13 -0300

    add: create and list users

--- login/scriptLogin.js
+++ login/scriptLogin.js
@@ -1,62 +1,86 @@
 const listar = async () => {
+  await fetch(`https://serverest.dev/usuarios`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((userList) => {
+      if (userList.length === 0) {
+        document.getElementById(
+          "resposta"
+        ).innerHTML = `<h5>Nenhum usuário cadastrado</h5>`;
+        return;
+      }
+      var cont = parseInt(userList.quantidade);
+      const parar = parseInt(cont);
+      var lista = [];
+      document.querySelector("#quantidade").innerText = `(${cont})`;
+      const pUsername = document.querySelector("#username");
+      const pUserEmail = document.querySelector("#userEmail");
 
-    if (false) {
-        document.getElementById('resposta').innerHTML = `<h5>O campo CEP deve ser preenchido</h5>`
-    } else {
-        await fetch(`https://serverest.dev/usuarios`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error: ${response.status}`);
-            }
-            return response.json();
-        }).then((usuarios) => {
-            if (usuarios.length === 0) {
-                document.getElementById('resposta').innerHTML = `<h5>CEP não encontrada</h5>`
-            } else {
-                console.log(usuarios)
-                var cont = parseInt(usuarios.quantidade);
-                const parar  = parseInt(cont);
-                var lista = []
-                for (i = 0; i < parar; i++){
-                    lista.push(i+1 + " " + usuarios.usuarios[i].nome) ;
-                }
+      for (i = 0; i < parar; i++) {
+        lista.push(i + 1 + " " + userList.usuarios[i].nome);
+      }
 
-                document.getElementById('resposta').innerHTML =
-                `<div class="card" style="width: 18rem;">
+      document.getElementById(
+        "resposta"
+      ).innerHTML = `<div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">${"Usuários"}</h5>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item"><b>Lista:</b> ${lista}</li>
                     </ul>
-                </div><br>`
-            }
-        })
-    }
+                </div><br>`;
+    });
 };
 
-const criar = async () => {
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
+listar();
 
-    if (false) {
-        document.getElementById('resposta').innerHTML = `<h5>O campo CEP deve ser preenchido</h5>`
-    } else {
-        await fetch(`https://serverest.dev/usuarios`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 
-            "nome": `${nome}`,
-            "email": `${email}`,
-            "password": "teste",
-            "administrador": "true" })
-        }).then(response => response.json())
-        .then((json) => console.log(json));
-    }
+const criar = async () => {
+  const nome = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  await fetch(`https://serverest.dev/usuarios`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: `${nome}`,
+      email: `${email}`,
+      password: "teste",
+      administrador: "true",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      if (!response) {
+        alert(`${response.message}`);
+        clearInputs();
+        return;
+      }
+      alert(`${response.message}`);
+      clearInputs();
+      window.location.reload();
+    })
+    .catch((error) => console.log(error));
+};
+
+const clearInputs = () => {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
 };
