2a57e548a100155b6277228ca3e9beae81d0db20
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-03-03 10:35:25 -0300

    add: structuring and styling the faq section

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+/* Script para mostrar as respostas de cada pergunta do FAQ */
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", () => {
+    console.log("TESTE");
+    coll[i].classList.toggle("active");
+    let content = coll[i].nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = `${content.scrollHeight}px`;
+    }
+  });
+}
