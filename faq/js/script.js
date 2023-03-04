af612c23cb37013679246a30b5c0541d27ad25df
Vinicius-b-q <borbaqueirozvinicius@gmail.com>
2023-03-04 00:33:00 -0300

    json server como armazenamento das msgs

--- faq/js/script.js
+++ faq/js/script.js
@@ -1,38 +1,76 @@
-const email = document.getElementById('userEmail');
-const nameUser = document.getElementById('userName');
+const emailUser = document.getElementById('userEmail');
+const nomeUser = document.getElementById('userName');
 const mensagens = document.getElementById('messages');
-const comentario = document.getElementById('comment');
+const comentarioUser = document.getElementById('comment');
 const date = new Date();
 const year = date.getFullYear();
 const day = date.getDate();
-const month = date.toLocaleDateString('PT-BR', { month: 'long' });
+const month = date.toLocaleDateString('PT-BR', { month: 'short' });
+const hour = date.getHours();
+const min = date.getMinutes();
 
 function postComment() {
-  console.log(month);
-  if (nameUser.value === '') {
+  if (nomeUser.value === '') {
     return alert('Digite o Nome !');
-  } else if (email.value === '') {
+  } else if (emailUser.value === '') {
     return alert('Digite o Email !');
   } else {
-    document.getElementById('messages').style.backgroundColor = ' #204945';
-    document.getElementById('hide').style.backgroundColor = 'red';
-    document.getElementById('postName').style.borderBottom =
-      ' 1px solid #3cbd96';
-    document.getElementById('postName').innerHTML = `${nameUser.value}`;
-    document.getElementById(
-      'dataPost'
-    ).innerHTML = `${day} de ${month} ${year}`;
-    document.getElementById('postEmail').innerHTML = ` ${email.value}`;
-    document.getElementById('postComent').innerHTML = `"${comentario.value}"`;
+    fetch('http://localhost:3000/coments', {
+      method: 'POST',
+      body: JSON.stringify({
+        nome: nomeUser.value,
+        email: emailUser.value,
+        comentario: comentarioUser.value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        nome = document.getElementById('postName');
+        email = document.getElementById('postEmail');
+        comentario = document.getElementById('postComent');
+        nome.innerHTML = data.nome;
+        email.innerHTML = data.email;
+        comentario.innerHTML = data.comentario;
+      })
+      .catch((error) => console.error('Error:', error));
   }
 }
 
-function cleanComent() {
-  document.getElementById('messages').style.backgroundColor = 'white';
-  document.getElementById('hide').style.backgroundColor = 'white';
-  document.getElementById('postName').style.borderBottom = ' white';
-  document.getElementById('postName').innerHTML = ``;
-  document.getElementById('dataPost').innerHTML = ``;
-  document.getElementById('postEmail').innerHTML = ` `;
-  document.getElementById('postComent').innerHTML = ``;
+async function addComents() {
+  const response = await fetch('http://localhost:3000/coments');
+
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+
+  data.map((response) => {
+    const div = document.createElement('div');
+    const namePost = document.createElement('h2');
+    const emailPost = document.createElement('p');
+    const comentPost = document.createElement('p1');
+    const dataPost = document.createElement('p3');
+    const horaPost = hour < 10 ? `0${hour}` : hour;
+    const minPost = min < 10 ? `0${min}` : min;
+    namePost.innerText = response.nome;
+    emailPost.innerText = response.email;
+    comentPost.innerText = `"${response.comentario}"`;
+    dataPost.innerHTML = `${day} de ${month} ${year}<br/>${horaPost}:${minPost}`;
+
+    div.appendChild(namePost);
+    div.appendChild(dataPost);
+    div.appendChild(emailPost);
+    div.appendChild(comentPost);
+
+    mensagens.appendChild(div);
+  });
 }
+
+addComents();
