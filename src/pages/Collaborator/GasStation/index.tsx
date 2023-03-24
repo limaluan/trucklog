ea1b9832998bcd160c3c9d9d81adc8a96961bf9f
Rafael <rafaelramos0401@gmail.com>
2023-03-24 00:30:23 -0300

    added modal and starte posting a new gasStation

--- src/pages/Collaborator/GasStation/index.tsx
+++ src/pages/Collaborator/GasStation/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useGasStations } from "../../../shared/hooks/useGasStations";
 import { GasStationContainer } from "./styles";
+import { CreateGasStationModal } from "../../../shared/components/Collaborator/Modals";
 
 export const Postos = () => {
   const { gasStations } = useGasStations();
   const [searchGasStation, setGasStation] = useState("");
-
-  console.log(gasStations);
+  const [isCreateTripModalOpen, setIsCreateTripModalOpen] = useState(false);
 
   return (
     <GasStationContainer>
@@ -18,7 +18,10 @@ export const Postos = () => {
         </div>
 
         <h2 className="title-page">Postos</h2>
-        <button className="create-button">
+        <button
+          onClick={() => setIsCreateTripModalOpen(true)}
+          className="create-button"
+        >
           Criar Posto <i className="ph ph-plus"></i>
         </button>
         <input
@@ -37,31 +40,47 @@ export const Postos = () => {
         </div>
 
         <div className="gas-station-body ">
-          {gasStations.map((gasStation) => (
-            <div
-              className={
-                gasStation.status === "ATIVO" ? "trip ativo" : "trip inativo"
-              }
-              key={gasStation.idPosto}
-            >
-              <p>{gasStation.nome}</p>
-              <p>
-                <p>
-                  {gasStation.valorCombustivel.toLocaleString("pt-br", {
-                    style: "currency",
-                    currency: "BRL",
-                  })}
-                </p>
-              </p>
-              <p
-                className={gasStation.status === "ATIVO" ? "ativo" : "inativo"}
+          {gasStations
+            .sort((item) => {
+              return item.status === "ATIVO" ? -1 : 1;
+            })
+
+            .filter((gasStation) =>
+              gasStation.nome
+                .toLowerCase()
+                .includes(searchGasStation.toLowerCase())
+            )
+            .map((gasStation) => (
+              <div
+                className={
+                  gasStation.status === "ATIVO" ? "trip ativo" : "trip inativo"
+                }
+                key={gasStation.idPosto}
               >
-                {gasStation.status}
-              </p>
-            </div>
-          ))}
+                <p>{gasStation.nome}</p>
+                <div>
+                  <p>
+                    {gasStation.valorCombustivel.toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </p>
+                </div>
+                <p
+                  className={
+                    gasStation.status === "ATIVO" ? "ativo" : "inativo"
+                  }
+                >
+                  {gasStation.status}
+                </p>
+              </div>
+            ))}
         </div>
       </main>
+      <CreateGasStationModal
+        isOpen={isCreateTripModalOpen}
+        onRequestClose={() => setIsCreateTripModalOpen(false)}
+      />
     </GasStationContainer>
   );
 };
--- src/shared/hooks/useGasStations.tsx
+++ src/shared/hooks/useGasStations.tsx
@@ -21,6 +21,12 @@ interface IGasStations {
 
 interface IGasStationContextData {
   gasStations: IGasStations[];
+  addNewGasStation: (gasStationdata: ICreateGasStation) => void;
+}
+
+interface ICreateGasStation {
+  nome: string;
+  valorCombustivel: number;
 }
 
 const GasStationsContext = createContext({} as IGasStationContextData);
@@ -36,8 +42,31 @@ export function GasStationProvider({
       .then((data) => setGasStations(data));
   }, []);
 
+  const addNewGasStation = async (gasStationdata: ICreateGasStation) => {
+    console.log("entrou", gasStationdata);
+    try {
+      const response = await fetch(api + `/posto?$idColaborador=${42}`, {
+        method: "POST",
+        headers: {
+          Authorization: "41",
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(gasStationdata),
+      });
+
+      if (response.ok) {
+        alert("Posto cadastrado");
+      } else {
+        alert("Ocorreu um erro ao cadastrar um posto");
+        console.log(response);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <GasStationsContext.Provider value={{ gasStations }}>
+    <GasStationsContext.Provider value={{ gasStations, addNewGasStation }}>
       {children}
     </GasStationsContext.Provider>
   );
