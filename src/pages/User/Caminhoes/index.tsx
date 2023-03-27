dd0f864e8835165c8d4be98f77149d802c4970bf
Luan Lima <limaluan.dev@gmail.com>
2023-03-27 00:07:07 -0300

    adds: fill up trucks functionality

--- src/pages/User/Caminhoes/index.tsx
+++ src/pages/User/Caminhoes/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import { CreateTruckModal } from "../../../shared/components/User/Modals/CreateTruckModal";
+import { CreateTruckModal } from "../../../shared/components/User/Modals/TruckModals/CreateTruckModal";
+import { EditTruckModal } from "../../../shared/components/User/Modals/TruckModals/EditTruckModal";
 import { useTrucks } from "../../../shared/hooks";
 import { CaminhoesContainer } from "./styles";
 
 export const Caminhoes = () => {
   const { trucks } = useTrucks();
 
+  const [truckId, setTruckId] = useState(0);
   const [searchTruck, setSearchTruck] = useState("");
 
   const [isCreateTruckModalOpen, setIsCreateTruckModalOpen] = useState(false);
+  const [isEditTruckModalOpen, setIsEditTruckModalOpen] = useState(false);
 
   return (
     <CaminhoesContainer>
@@ -49,10 +52,23 @@ export const Caminhoes = () => {
               truck.modelo.toLowerCase().includes(searchTruck.toLowerCase())
             )
             .map((truck) => (
-              <div className={`truck truck-${truck.status.toLowerCase()}`} key={truck.placa}>
+              <div
+                className={`truck truck-${truck.status.toLowerCase()}`}
+                key={truck.placa}
+              >
                 <p>{truck.modelo}</p>
                 <p>{truck.placa}</p>
-                <p>{truck.nivelCombustivel}%</p>
+                <p
+                  className={
+                    truck.nivelCombustivel <= 20
+                      ? "error"
+                      : truck.nivelCombustivel <= 60
+                      ? "warning"
+                      : "success"
+                  }
+                >
+                  {truck.nivelCombustivel}%
+                </p>
                 <p
                   className={
                     truck.statusCaminhao === "EM_VIAGEM" ? "warning" : "success"
@@ -64,9 +80,23 @@ export const Caminhoes = () => {
                   className={truck.status === "INATIVO" ? "error" : "success"}
                 >
                   {truck.status}
-                  <button>
-                    <i className="ph ph-pencil"></i>
-                  </button>
+                  <div className="btn-container">
+                    <button
+                      onClick={() => {
+                        setTruckId(truck.idCaminhao);
+                        return setIsEditTruckModalOpen(true);
+                      }}
+                    >
+                      <i title="Abastecer" className="ph ph-gas-pump"></i>
+                    </button>
+
+                    <button>
+                      <i
+                        title="Deletar"
+                        className="ph ph-trash delete-icon"
+                      ></i>
+                    </button>
+                  </div>
                 </div>
               </div>
             ))}
@@ -75,6 +105,11 @@ export const Caminhoes = () => {
       <CreateTruckModal
         isOpen={isCreateTruckModalOpen}
         onRequestClose={() => setIsCreateTruckModalOpen(false)}
+      />
+      <EditTruckModal
+        isOpen={isEditTruckModalOpen}
+        onRequestClose={() => setIsEditTruckModalOpen(false)}
+        truckId={truckId}
       />
     </CaminhoesContainer>
   );
--- src/shared/components/User/Modals/index.ts
+++ src/shared/components/User/Modals/index.ts
@@ -6,7 +6,7 @@ export * from "./DriverModals/DeleteDriverModal";
 export * from "./DriverModals/EditDriverModal";
 
 //Truck Modals
-export * from "./CreateTruckModal";
+export * from "./TruckModals/CreateTruckModal";
 
 //Gas Station Modals
 export * from "./GasStationModals/CreateGasStationModal";
