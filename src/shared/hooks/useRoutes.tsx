fca696363bdc6c2256ad32e5f81d0bece019725a
Luan Lima <limaluan.dev@gmail.com>
2023-03-27 14:25:35 -0300

    hotfix: useEffect import on useRoutes

--- src/shared/hooks/useRoutes.tsx
+++ src/shared/hooks/useRoutes.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
