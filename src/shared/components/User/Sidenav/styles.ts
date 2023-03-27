5a9970244ff8b2682c311cf6f7f234a7613a1dfa
Luan Lima <limaluan.dev@gmail.com>
2023-03-27 01:44:39 -0300

    feat: adds toggleOpenSidenav Functionality

--- src/shared/components/User/Sidenav/styles.ts
+++ src/shared/components/User/Sidenav/styles.ts
@@ -1,16 +1,51 @@
 import styled from "styled-components";
 
 export const SidenavContainer = styled.nav`
-  width: 28rem;
+  .container {
+    display: "flex";
+    margin-left: 18rem;
+    transition: margin-left 200ms;
+
+    &.expanded {
+      margin-left: 0;
+    }
+  }
+
+  /* @media (max-width: 600px) {
+    .container {
+      margin-left: 0;
+
+      &.expanded::before {
+        content: "";
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.2);
+        z-index: 2;
+      }
+    }
+  } */
+`;
+
+export const SidenavContent = styled.nav`
+  &.expanded {
+    left: 0rem;
+    transition: left 200ms;
+  }
+
+  position: absolute;
+  left: -20rem;
+  width: 20rem;
   height: 90vh;
   border-right: 2px solid ${(props) => props.theme["gray-100"]};
+  background-color: #fff;
+  z-index: 2;
 
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
 
   padding: 2rem;
-  transition: all 200ms;
 
   .category {
     font-size: 1.2rem;
